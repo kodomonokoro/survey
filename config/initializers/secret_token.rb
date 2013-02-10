be5499f83a98b635625f056e1ff5615f5651b102
Minagawa Dai <d-minagawa@nri.co.jp>
2013-02-10 16:42:48 +0900

    初期状態

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = 'c7570c3367ab99a44cb3ec96d7f317f81d3029fa97eec12711ea30a6ef60eeee1eaf7d9a21af5573d733964fe1c9750b351508b41ef24602bfbd6ff3579dbdca'
